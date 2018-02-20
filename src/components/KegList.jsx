e6e3562546c4aabed4040c0137fbf9695786e6f0
Gyles Batara <gyles.batara@live.com>
2018-02-20 13:12:46 -0800

    Back to Week 1 project

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,76 +1,55 @@
 import React from 'react';
 import Keg from './Keg';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-//
-//
-// const masterList = [
-//   {
-//     name: 'Guiness Draught',
-//     brand: 'Guiness',
-//     price: '205',
-//     alc: '4.20'
-//   },
-//   {
-//     name: 'Blue Moon - Belgian White',
-//     brand: 'Blue Moon',
-//     price: '150',
-//     alc: '5.40'
-//   },
-//   {
-//     name: 'Mannys Pale Ale',
-//     brand: 'Mannys',
-//     price: '140',
-//     alc: '5.40'
-//   },
-//   {
-//     name: 'Deschutes IPA',
-//     brand: 'Deschutes',
-//     price: '190',
-//     alc: '7.00'
-//   },
-// ];
-//
-// function KegList(){
-//   return (
-//     <div style={{padding: '15px'}}>
-//       <h2 style={{color: '#206040'}}>Kegs</h2>
-//       <div className="row">
-//         {masterList.map((kegList, index) =>
-//           <div className="col-md-4">
-//             <Keg
-//               name={kegList.name}
-//               brand={kegList.brand}
-//               price={kegList.price}
-//               alc={kegList.alc}
-//               key={index}/>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//
-//   );
-// }
 
-function KegList(props){
+
+const masterList = [
+  {
+    name: 'Guiness Draught',
+    brand: 'Guiness',
+    price: '205',
+    alc: '4.20'
+  },
+  {
+    name: 'Blue Moon - Belgian White',
+    brand: 'Blue Moon',
+    price: '150',
+    alc: '5.40'
+  },
+  {
+    name: 'Mannys Pale Ale',
+    brand: 'Mannys',
+    price: '140',
+    alc: '5.40'
+  },
+  {
+    name: 'Deschutes IPA',
+    brand: 'Deschutes',
+    price: '190',
+    alc: '7.00'
+  },
+];
+
+function KegList(){
   return (
-    <div style = {{padding: '15px'}}>
-      <h2 style = {{color: '#206040'}}>Kegs</h2>
-      <div className = "row">
-        {props.kegList.map((keg) =>
-          <Keg names = {keg.name}
-            brand = {keg.brand}
-            price = {keg.brand}
-            alc = {keg.brand}
-            key = {keg.id}/>
+    <div style={{padding: '15px'}}>
+      <h2 style={{color: '#206040'}}>Kegs</h2>
+      <div className="row">
+        {masterList.map((kegList, index) =>
+          <div className="col-md-4">
+            <Keg
+              name={kegList.name}
+              brand={kegList.brand}
+              price={kegList.price}
+              alc={kegList.alc}
+              key={index}/>
+          </div>
         )}
       </div>
     </div>
+
   );
 }
-
-KegList.propTypes = {
-  kegList: PropTypes.array
-};
 
 export default KegList;
